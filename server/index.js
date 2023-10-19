d3c294194fd8768139fbad5961105f6df79ec531
NicolasPower <47236019+NicolasPower@users.noreply.github.com>
2023-10-19 11:16:03 +1000

    started work on backend proper. finished content told within the tutorial link

---
+++ server/index.js
@@ -0,0 +1,43 @@
+require("dotenv").config();
+const express = require("express");
+const cloudinary = require("cloudinary").v2;
+const cors = require("cors");
+const Multer = require("multer");
+
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET,
+  });
+  async function handleUpload(file) {
+    const res = await cloudinary.uploader.upload(file, {
+      resource_type: "auto",
+    });
+    return res;
+  }
+
+const storage = new Multer.memoryStorage();
+const upload = Multer({
+  storage,
+});
+
+const app = express();
+app.use(cors());
+
+app.post("/upload", upload.single("my_file"), async (req, res) => {
+  try {
+    const b64 = Buffer.from(req.file.buffer).toString("base64");
+    let dataURI = "data:" + req.file.mimetype + ";base64," + b64;
+    const cldRes = await handleUpload(dataURI);
+    res.json(cldRes);
+  } catch (error) {
+    console.log(error);
+    res.send({
+      message: error.message,
+    });
+  }
+});
+const port = 6060;
+app.listen(port, () => {
+  console.log(`Server Listening on ${port}`);
+});
