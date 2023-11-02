6dbdb9d03a278dbc0cbb628c3d09666c40c97c53
NicolasPower <nicolas.power@optusnet.com.au>
2023-11-02 11:00:12 +1000

    allows users to upload multiple images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,14 +15,40 @@ function App() {
   const [processing, setProcessing] = useState(false);
   const [selectedFormat, setSelectedFormat] = useState("9:16 Instagram Story");
 
+  // const handleSelectFile = (e) => {
+  //   const files = e.target.files;
+  //   if (files && files.length > 0 && files[0] instanceof Blob) {
+  //     console.log(files)
+  //     setFile(files[0]);
+  //     setOriginalUrl(URL.createObjectURL(files[0]));
+  //     setImageUrl("");
+  //   } else {
+  //     console.error("No valid file selected");
+  //   }
+  // };
+
   const handleSelectFile = (e) => {
     const files = e.target.files;
-    if (files && files.length > 0 && files[0] instanceof Blob) {
-      setFile(files[0]);
-      setOriginalUrl(URL.createObjectURL(files[0]));
-      setImageUrl("");
+    if (files && files.length > 0) {
+      const fileArray = [];
+      for (let i = 0; i < files.length; i++) {
+        if (files[i] instanceof Blob) {
+          const file = files[i];
+          const url = URL.createObjectURL(file);
+          fileArray.push({ file, url });
+        }
+      }
+      if (fileArray.length > 0) {
+        // Do something with the array of selected files
+        console.log(fileArray);
+        setFile(fileArray); // Assuming you have a state variable to store the selected files
+        setOriginalUrl(URL.createObjectURL(files[0]));
+        setImageUrl("");
+      } else {
+        console.error("No valid files selected");
+      }
     } else {
-      console.error("No valid file selected");
+      console.error("No files selected");
     }
   };
 
@@ -30,11 +56,20 @@ function App() {
     try {
       setProcessing(true);
       setImageUrl("");
-      const data = new FormData();
-      data.append("my_file", file);
-      data.append("format", selectedFormat);
-      const uploadRes = await axios.post("http://localhost:6060/upload", data);
-      setPhotoId(uploadRes.data.photoId);
+      
+      const photoIds = []; // Array to store the photo IDs for each file
+      
+      for (const selectedFile of file) { // Assuming you have an array named "files" with the selected files
+        const data = new FormData();
+        data.append("my_file", selectedFile.file);
+        data.append("format", selectedFormat);
+        
+        const uploadRes = await axios.post("http://localhost:6060/upload", data);
+        console.log(data)
+        photoIds.push(uploadRes.data.photoId);
+      }
+      
+      setPhotoId(photoIds); // Assuming you have a state variable to store the uploaded photo IDs
     } catch (error) {
       alert(error.message);
       setProcessing(false);
@@ -161,7 +196,7 @@ function App() {
               id="file"
               type="file"
               onChange={handleSelectFile}
-              multiple={false}
+              multiple={true}
               accept=".jpeg, .jpg"
               className="form-control d-none"
             />
