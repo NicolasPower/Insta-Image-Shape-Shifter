8d01ab8482564996d0264b9b0547abce217e6e72
NicolasPower <nicolas.power@optusnet.com.au>
2023-11-04 17:11:49 +1000

    added docker stuff

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require("dotenv").config({ path: `${__dirname}/.env` });
 const axios = require("axios");
 const express = require("express");
 const cors = require("cors");
